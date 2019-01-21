62327c5ee151042631756eaa424f8e6cbcf5b263
Serg M <js.develop.com@gmail.com>
2019-01-21 18:11:22 +0200

    refactor the code

--- components/cookies-handler/index.jsx
+++ components/cookies-handler/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Button, CustomInput, Form, FormGroup } from 'reactstrap'
 import './cookies-handler.scss'
 import { bind } from 'decko'
@@ -13,69 +13,65 @@ class CookiesHandler extends React.Component {
   }
 
   render() {
-    const { isOpen } = this.state
+    if (!this.state.isOpen) return null
     return (
-      <Fragment>
-        {isOpen && !null && (
-          <div className="cookies-settings p-3">
-            <span className="cookies-header m-1">Cookie settings</span>
-            <Button
-              id="apply-cookies-btn1"
-              color="primary"
-              type="button"
-              title="Apply cookies"
-              onClick={this.applyCookies}
-              outline
-            >
-              Save
-            </Button>
+      <div className="cookies-settings p-3">
+        <span className="cookies-header m-1">Cookie settings</span>
+        <Button
+          id="apply-cookies-btn1"
+          color="primary"
+          type="button"
+          title="Apply cookies"
+          onClick={this.applyCookies}
+          outline
+        >
+          Save
+        </Button>
 
-            <div className="py-3">
-              <Form>
-                <FormGroup>
-                  <CustomInput
-                    id="essential-cookies"
-                    type="switch"
-                    label="Essential cookies"
-                    checked
-                    disabled
-                  />
-                  <details className="ml-4">
-                    <summary>What are these?</summary>
-                    <p>
-                      <strong>Required</strong> to make the site work, and to
-                      save the options you make here
-                    </p>
-                  </details>
-                </FormGroup>
-                <FormGroup>
-                  <CustomInput id="analytics" type="switch" label="Analytics" />
-                  <details className="ml-4">
-                    <summary>What are these?</summary>
-                    <p>
-                      Lets us (anonymously) track site usage, so that we can
-                      measure performance and make improvements.
-                    </p>
-                  </details>
-                </FormGroup>
-              </Form>
-              <Button
-                id="apply-cookies-btn2"
-                title="Apply cookies"
-                onClick={this.applyCookies}
-                color="primary"
-                type="button"
-                className="d-block my-3 ml-3"
-              >
-                Save
-              </Button>
-              <a href="https://www.google.com/">
-                Read more about how we use cookies
-              </a>
-            </div>
-          </div>
-        )}
-      </Fragment>
+        <div className="py-3">
+          <Form>
+            <FormGroup>
+              <CustomInput
+                id="essential-cookies"
+                type="switch"
+                label="Essential cookies"
+                checked
+                disabled
+              />
+              <details className="ml-4">
+                <summary>What are these?</summary>
+                <p>
+                  <strong>Required</strong> to make the site work, and to save
+                  the options you make here
+                </p>
+              </details>
+            </FormGroup>
+            <FormGroup>
+              <CustomInput id="analytics" type="switch" label="Analytics" />
+              <details className="ml-4">
+                <summary>What are these?</summary>
+                <p>
+                  Lets us (anonymously) track site usage, so that we can measure
+                  performance and make improvements.
+                </p>
+              </details>
+            </FormGroup>
+          </Form>
+          <Button
+            id="apply-cookies-btn2"
+            title="Apply cookies"
+            onClick={this.applyCookies}
+            color="primary"
+            type="button"
+            className="d-block my-3 ml-3"
+          >
+            Save
+          </Button>
+          <a href="https://www.google.com/">
+            Read more about how we use cookies
+          </a>
+        </div>
+      </div>
     )
   }
 }
