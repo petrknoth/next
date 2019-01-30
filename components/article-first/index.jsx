976dbacd3e16404ac040a6535ae5d5efa79c52fc
Serg M <js.develop.com@gmail.com>
2019-01-30 12:20:43 +0200

    create Article component

---
+++ components/article-first/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import './article-first.scss'
+import {
+  Row,
+  Col,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  CardText,
+} from 'reactstrap'
+import { Content, Section } from 'components'
+import researchData from '../../data/research.yml'
+
+const ArticleFirst = () => (
+  <Section id="all-articles" caption="All articles">
+    <h2>{researchData.allarticles.title}</h2>
+
+    <Section id="core-vision" caption="CORE vision">
+      <h3>{researchData.allarticles.vision.title}</h3>
+      <Row>
+        <Col xs="12" lg="9">
+          <Card className="card-border">
+            <CardHeader className="card-header">
+              <Content markdown>
+                {researchData.allarticles.vision.cardheader}
+              </Content>
+            </CardHeader>
+            <CardBody color="primary">
+              <CardTitle>
+                <Content markdown>
+                  {researchData.allarticles.vision.cardtitle}
+                </Content>
+              </CardTitle>
+              <CardText>
+                <Content markdown>
+                  {researchData.allarticles.vision.cardtext}
+                </Content>
+              </CardText>
+              <UncontrolledDropdown className="text-right">
+                <DropdownToggle caret color="primary" outline>
+                  Cite
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>BibTex</DropdownItem>
+                  <DropdownItem>Full citation</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Section>
+  </Section>
+)
+
+export default ArticleFirst
--- pages/research-output/index.jsx
+++ pages/research-output/index.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import './research.scss'
-import {
-  Row,
-  Col,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  Card,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  CardText,
-} from 'reactstrap'
+import { Row, Col } from 'reactstrap'
 import { Article, Content, Section } from 'components'
 import researchData from '../../data/research.yml'
+import ArticleFirst from '../../components/article-first'
 
 const ResearchOutput = () => (
   <Article nav tag="main">
@@ -29,45 +18,7 @@ const ResearchOutput = () => (
       </Row>
     </Section>
 
-    <Section id="all-articles" caption="All articles">
-      <h2>{researchData.allarticles.title}</h2>
-
-      <Section id="core-vision" caption="CORE vision">
-        <h3>{researchData.allarticles.vision.title}</h3>
-        <Row>
-          <Col xs="12" lg="9">
-            <Card className="card-border">
-              <CardHeader className="card-header">
-                <Content markdown>
-                  {researchData.allarticles.vision.cardheader}
-                </Content>
-              </CardHeader>
-              <CardBody color="primary">
-                <CardTitle>
-                  <Content markdown>
-                    {researchData.allarticles.vision.cardtitle}
-                  </Content>
-                </CardTitle>
-                <CardText>
-                  <Content markdown>
-                    {researchData.allarticles.vision.cardtext}
-                  </Content>
-                </CardText>
-                <UncontrolledDropdown className="text-right">
-                  <DropdownToggle caret color="primary" outline>
-                    Cite
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem>BibTex</DropdownItem>
-                    <DropdownItem>Full citation</DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </Section>
-    </Section>
+    <ArticleFirst />
   </Article>
 )
 
