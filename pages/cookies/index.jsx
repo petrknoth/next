83cd845fc4e8ebf752131ec058eafc21966d091e
Serg M <js.develop.com@gmail.com>
2019-02-05 16:50:08 +0200

    add cookies page

---
+++ pages/cookies/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Row, Col } from 'reactstrap'
+import { Article, Content, Section } from 'components'
+import cookiesData from 'data/cookies.yml'
+
+const CookiesPage = () => (
+  <Article container nav tag="main">
+    <h1>{cookiesData.title}</h1>
+
+    <Section id="cookie-setting" caption="Your cookie settings" tag="div">
+      <Row>
+        <Col xs="12" lg="9" tag="section">
+          <p className="h5">{cookiesData.settings.title}</p>
+          <Content markdown>{cookiesData.settings.description}</Content>
+          <Content markdown>{cookiesData.settings.option1.title}</Content>
+          <Content markdown>{cookiesData.settings.option1.description}</Content>
+          <Content markdown>{cookiesData.settings.option2.title}</Content>
+          <Content markdown>{cookiesData.settings.option2.description}</Content>
+          <Content markdown>{cookiesData.settings.option3.title}</Content>
+          <Content markdown>{cookiesData.settings.option3.description}</Content>
+        </Col>
+      </Row>
+    </Section>
+
+    <Section id="use-cookies" caption="How we use cookies" tag="div">
+      <Row>
+        <Col xs="12" lg="9" tag="section">
+          <h3>{cookiesData.use.title}</h3>
+          <Content markdown>{cookiesData.use.description}</Content>
+          <Content markdown>{cookiesData.use.option1.title}</Content>
+          <Content markdown>{cookiesData.use.option1.description}</Content>
+          <Content markdown>{cookiesData.use.option2.title}</Content>
+          <Content markdown>{cookiesData.use.option2.description}</Content>
+          <Content markdown>{cookiesData.use.option3.title}</Content>
+          <Content markdown>{cookiesData.use.option3.description}</Content>
+        </Col>
+      </Row>
+    </Section>
+
+    <Section id="privacy-notice" caption="Privacy notice" tag="div">
+      <Row>
+        <Col xs="12" lg="9" tag="section">
+          <h3>{cookiesData.privacy.title}</h3>
+          <Content markdown>{cookiesData.privacy.description}</Content>
+        </Col>
+      </Row>
+    </Section>
+  </Article>
+)
+
+export default CookiesPage
