da917b5aa9f0aaa1e11cbdd4021908eff0743968
Serg M <js.develop.com@gmail.com>
2019-02-18 18:13:32 +0200

    fix buttons

--- pages/services/rd.jsx
+++ pages/services/rd.jsx
@@ -10,81 +10,77 @@ import Testimonial from '../../components/testimonial'
 const RdPage = () => (
   <Article nav>
     <h1>{rdData.title}</h1>
-    <Section caption={rdData.short.subtitle}>
-      <p className="h4 text-center">{rdData.short.subtitle}</p>
-      <figure>
-        <img
-          className="img-fluid"
-          src={rdData.short.screenshot}
-          alt={`${rdData.short.subtitle}'s screenshot`}
-        />
-        <figcaption className="text-center">
-          <Content markdown>{rdData.short.caption}</Content>
-        </figcaption>
-      </figure>
+    <p className="h4 text-center">{rdData.tagline}</p>
 
-      <Row className="pt-5">
-        <Col xs="12" md="4">
-          <figure className="d-flex align-items-center justify-content-center service-section-logo">
-            <img
-              className="img-fluid"
-              src={rdData.short.logo1}
-              alt={`${rdData.short.logo1}'s logo`}
-            />
-          </figure>
-          <span className="h6 text-center d-block">
-            {rdData.short.caption1}
-          </span>
-        </Col>
+    <figure>
+      <img
+        className="img-fluid"
+        src={rdData.screenshot.source}
+        alt={`${rdData.title}'s screenshot`}
+      />
+      <figcaption className="text-center">
+        <Content markdown>{rdData.screenshot.caption}</Content>
+      </figcaption>
+    </figure>
 
-        <Col xs="12" md="4">
-          <figure className="d-flex align-items-center justify-content-center service-section-logo">
-            <img
-              className="img-fluid"
-              src={rdData.short.logo2}
-              alt={`${rdData.short.logo2}'s logo`}
-            />
-          </figure>
-          <span className="h6 text-center d-block">
-            {rdData.short.caption2}
-          </span>
-        </Col>
+    <Row className="pt-5">
+      <Col xs="12" md="4">
+        <figure className="d-flex align-items-center justify-content-center service-section-logo">
+          <img
+            className="img-fluid"
+            src={rdData.logo1}
+            alt={`${rdData.logo1}'s logo`}
+          />
+        </figure>
+        <span className="h6 text-center d-block">{rdData.caption1}</span>
+      </Col>
 
-        <Col xs="12" md="4">
-          <figure className="d-flex align-items-center justify-content-center service-section-logo">
-            <img
-              className="img-fluid"
-              src={rdData.short.logo3}
-              alt={`${rdData.short.logo3}'s logo`}
-            />
-          </figure>
-          <span className="h6 text-center d-block">
-            {rdData.short.caption3}
-          </span>
-        </Col>
-      </Row>
+      <Col xs="12" md="4">
+        <figure className="d-flex align-items-center justify-content-center service-section-logo">
+          <img
+            className="img-fluid"
+            src={rdData.logo2}
+            alt={`${rdData.logo2}'s logo`}
+          />
+        </figure>
+        <span className="h6 text-center d-block">{rdData.caption2}</span>
+      </Col>
 
-      <Content className="py-5" markdown>
-        {rdData.short.description}
-      </Content>
+      <Col xs="12" md="4">
+        <figure className="d-flex align-items-center justify-content-center service-section-logo">
+          <img
+            className="img-fluid"
+            src={rdData.logo3}
+            alt={`${rdData.logo3}'s logo`}
+          />
+        </figure>
+        <span className="h6 text-center d-block">{rdData.caption3}</span>
+      </Col>
+    </Row>
 
-      <Testimonial {...rdData.short.testimonial} />
-    </Section>
+    <Content className="py-5" markdown>
+      {rdData.description}
+    </Content>
 
-    <Section
-      caption={rdData.package.title}
-      key={rdData.package.title}
-      id={rdData.package.title}
-    >
+    <Testimonial {...rdData.testimonial} />
+
+    <Section caption={rdData.package.title} id="#what-is-included">
       <h2>{rdData.package.title}</h2>
-      <p className="h4 ">{rdData.package.subtitle}</p>
       <Content markdown>{rdData.package.description}</Content>
       <div className="text-center">
-        <Button color="primary" outline href={rdData.package.button1}>
-          {rdData.package.caption1}
+        <Button
+          color="primary"
+          outline
+          href={rdData.package.actions.primary.url}
+        >
+          {rdData.package.actions.primary.caption}
         </Button>
-        <Button color="primary" href={rdData.package.button2} className="ml-2">
-          {rdData.package.caption2}
+        <Button
+          color="primary"
+          href={rdData.package.actions.secondary.url}
+          className="ml-2"
+        >
+          {rdData.package.actions.secondary.caption}
         </Button>
       </div>
     </Section>
@@ -98,11 +94,19 @@ const RdPage = () => (
       <p className="h4 ">{rdData.premium.subtitle}</p>
       <Content markdown>{rdData.premium.description}</Content>
       <div className="text-center">
-        <Button color="primary" outline href={rdData.premium.button1}>
-          {rdData.premium.caption1}
+        <Button
+          color="primary"
+          outline
+          href={rdData.premium.actions.primary.url}
+        >
+          {rdData.premium.actions.primary.caption}
         </Button>
-        <Button color="primary" href={rdData.premium.button2} className="ml-2">
-          {rdData.premium.caption2}
+        <Button
+          color="primary"
+          href={rdData.premium.actions.secondary.url}
+          className="ml-2"
+        >
+          {rdData.premium.actions.secondary.caption}
         </Button>
       </div>
     </Section>
@@ -116,15 +120,19 @@ const RdPage = () => (
       <p className="h4 ">{rdData.documentation.subtitle}</p>
       <Content markdown>{rdData.documentation.description}</Content>
       <div className="text-center">
-        <Button color="primary" outline href={rdData.documentation.button1}>
-          {rdData.documentation.caption1}
+        <Button
+          color="primary"
+          outline
+          href={rdData.documentation.actions.primary.url}
+        >
+          {rdData.documentation.actions.primary.caption}
         </Button>
         <Button
           color="primary"
-          href={rdData.documentation.button2}
+          href={rdData.documentation.actions.secondary.url}
           className="ml-2"
         >
-          {rdData.documentation.caption2}
+          {rdData.documentation.actions.secondary.caption}
         </Button>
       </div>
     </Section>
