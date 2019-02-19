83a2656d890debff87d5abd62f9942136f82c726
Serg M <js.develop.com@gmail.com>
2019-02-19 16:04:19 +0200

    create services page component

---
+++ components/services-pages/index.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import {
+  Article,
+  Content,
+  Section,
+  Button,
+  KeyFeatureList,
+  KeyFeature,
+} from 'components'
+
+import './services-pages.scss'
+import Testimonial from '../testimonial'
+
+const ServicesPages = ({
+  title,
+  tagline,
+  screenshot,
+  features,
+  testimonial,
+  freePackage,
+}) => (
+  <Article nav>
+    <h1>{title}</h1>
+    <p className="lead">{tagline}</p>
+
+    <figure>
+      <img
+        className="img-fluid"
+        src={screenshot.source}
+        alt={`${title}'s screenshot`}
+      />
+      <figcaption className="text-center">
+        <Content markdown>{screenshot.caption}</Content>
+      </figcaption>
+    </figure>
+
+    <KeyFeatureList className="pt-5">
+      {features.map(feature => (
+        <KeyFeature
+          title={feature.title}
+          icon={feature.picture}
+          key={feature.title}
+        >
+          <Content markdown>{feature.title}</Content>
+        </KeyFeature>
+      ))}
+    </KeyFeatureList>
+
+    <Content className="py-5" markdown>
+      {features.description}
+    </Content>
+
+    <Testimonial {...testimonial} />
+
+    <Section caption={freePackage.title} id="#what-is-included">
+      <h2>{freePackage.title}</h2>
+      <Content markdown>{freePackage.description}</Content>
+      <div className="text-center">
+        <Button color="primary" outline href={freePackage.actions.primary.url}>
+          {freePackage.actions.primary.caption}
+        </Button>
+        <Button
+          color="primary"
+          href={freePackage.actions.secondary.url}
+          className="ml-2"
+        >
+          {freePackage.actions.secondary.caption}
+        </Button>
+      </div>
+    </Section>
+  </Article>
+)
+
+export default ServicesPages
---
+++ pages/sp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import ServicesPages from 'components/services-pages'
+
+import textData from 'data/rd.yml'
+
+const SP = () => <ServicesPages {...textData} />
+
+export default SP
