7af5f2010029109d5384843667cf004783e8ef95
Serg M <js.develop.com@gmail.com>
2019-02-19 16:15:58 +0200

    split free-package

--- pages/sp.jsx
+++ pages/sp.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 import ServicesPages from 'components/services-pages'
 
 import textData from 'data/rd.yml'
+import freePackage from 'data/free-package.yml'
 
-const SP = () => <ServicesPages {...textData} />
+const SP = () => <ServicesPages freePackage={freePackage} {...textData} />
 
 export default SP
